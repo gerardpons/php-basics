20eee8a83479a735f67b1d6484ff342661b64242
Gerard Pons <gpguisado@gmail.com>
2021-11-23 09:31:06 +0100

    Nivell 1 - Exercici 1

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+    $nombre = 'Gerard'; // declaro variable string
+    $edad = 29; // declaro variable integer
+    $precio = 79.99; // declaro variable float/double
+    $pagado = true; // declaro variable boolean
+
+    echo $nombre;
+    echo '<br>';
+    echo $edad;
+    echo '<br>';
+    echo $precio;
+    echo '<br>';
+    echo $pagado;
+
+?>
