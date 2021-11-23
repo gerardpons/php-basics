2d2ad3c523d82460e06f2cbc67cd755ed9a0d4f9
Gerard Pons <gpguisado@gmail.com>
2021-11-23 09:41:12 +0100

    Nivell 1 - Exercici 2

---
+++ exercici2.php
@@ -0,0 +1,16 @@
+<?php
+
+    $message = 'Hello, World!';
+    $message2 = ' Aquest és el curs de PHP';
+
+    echo $message;
+    echo '<br>';
+    echo strtoupper($message);
+    echo '<br>';
+    echo strlen($message);
+    echo '<br>';
+    echo strrev($message);
+    echo '<br>';
+    echo $message.$message2;
+
+?>
