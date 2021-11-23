35a5236483dc6884e1b3dbeebe0a58d1ff8695bf
Gerard Pons <gpguisado@gmail.com>
2021-11-23 12:05:13 +0100

    Nivell 1 - Exercici 4

---
+++ exercici4.php
@@ -0,0 +1,51 @@
+<?php
+
+    $X = 10;
+    $Y = 5;
+
+    $N = 10.50;
+    $M = 7.25;
+
+    echo $X;
+    echo '<br>';
+    echo $Y;
+    echo '<br>';
+    echo $X + $Y;
+    echo '<br>';
+    echo $X - $Y;
+    echo '<br>';
+    echo $X * $Y;
+    echo '<br>';
+    echo $X % $Y;
+
+    echo '<br>';
+    echo '<br>';
+
+    echo $N;
+    echo '<br>';
+    echo $M;
+    echo '<br>';
+    echo $N + $M;
+    echo '<br>';
+    echo $N - $M;
+    echo '<br>';
+    echo $N * $M;
+    echo '<br>';
+    echo $N % $M;
+
+    echo '<br>';
+    echo '<br>';
+
+    echo $X * 2;
+    echo '<br>';
+    echo $Y * 2;
+    echo '<br>';
+    echo $N * 2;
+    echo '<br>';
+    echo $M * 2;
+    echo '<br>';
+    echo $X + $Y + $N + $M;
+    echo '<br>';
+    echo $X * $Y * $N * $M; 
+
+?>
